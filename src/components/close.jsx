25cd92f72ce8dbfaec7186a88b3589dc3c2116ac
Forslunddavid <forslund.david@gmail.com>
2025-04-25 17:45:48 +0200

    Updates

--- src/components/close.jsx
+++
@@ -1,6 +0,0 @@
-import "./attributeboxes.css"
-
-const Close = () => {
-	return <p className="box close-attribute">Close</p>
-}
-export default Close
--- src/constants/endpoints.js
+++
@@ -1,5 +0,0 @@
-/movie/popular
-/search/movie
-/movie/{id}
-/discover
-/find
