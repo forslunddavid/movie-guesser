3b1d08022ec0690da436a2ec58a468db615e5534
Forslunddavid <forslund.david@gmail.com>
2025-04-29 12:31:04 +0200

    info box fix

--- src/components/howToPlay.jsx
+++ src/components/howToPlay.jsx
@@ -2,14 +2,27 @@ import "./howToPlay.css"
 
 const HowToPlay = () => {
 	return (
-		<p>
-			Guess the artist in 10 tries. Each guess reveals the attributes of
-			artist. The color of the tiles will change to show how close your
-			guess was. Step 1 Guess an artist using the search bar. How to play
-			diagram 1 Step 2 Make another guess based off the new information.
-			How to play diagram 2 Step 3 Keep going until you guess the artist.
-			A new Spotle will appear every day! How to play diagram 3
-		</p>
+		<div className="how-to-play">
+			<h3>How to Play MovieGrid</h3>
+			<p>
+				Guess the movie in 10 tries. Each guess reveals the attributes of
+				the movie. The color of the tiles will change to show how close your
+				guess was.
+			</p>
+			<h4>Step 1</h4>
+			<p>Guess a movie using the search bar.</p>
+			<div className="example-box">[How to play diagram 1]</div>
+			
+			<h4>Step 2</h4>
+			<p>Make another guess based on the new information.</p>
+			<div className="example-box">[How to play diagram 2]</div>
+			
+			<h4>Step 3</h4>
+			<p>Keep going until you guess the movie.</p>
+			<div className="example-box">[How to play diagram 3]</div>
+			
+			<p className="note">A new MovieGrid will appear every day!</p>
+		</div>
 	)
 }
 
