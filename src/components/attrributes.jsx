7dc367cb858581a7ce68e74f697aa8830c919888
Forslunddavid <forslund.david@gmail.com>
2025-05-09 16:53:16 +0200

    fix z-index for header

--- src/components/attrributes.jsx
+++ src/components/attrributes.jsx
@@ -2,11 +2,16 @@ import "./attributes.css"
 import Incorrect from "./incorrect.jsx"
 import Correct from "./correct.jsx"
 import Close from "./close.jsx"
+import { ArrowUp, ArrowDown } from "lucide-react"
 
 const Attributes = () => {
 	return (
 		<>
 			<p>The year the movie was released</p>
+			<p>
+				If you are within 3 years it will show close (yellow) and a
+				direction arrow <ArrowUp /> or <ArrowDown />
+			</p>
 			<div className="attribute-boxes">
 				<Incorrect /> <Close />
 				<Correct />
@@ -16,18 +21,26 @@ const Attributes = () => {
 				<Incorrect />
 				<Correct />
 			</div>
-			<p>The year the movie was released</p>{" "}
+			<p>The genre of the movie</p>{" "}
 			<div className="attribute-boxes">
-				<Correct /> <Close />
+				<Correct />
 				<Incorrect />
 			</div>
 			<p>The budget of the movie </p>
+			<p>
+				If you are within 20% of the movies budget it will show close
+				(yellow) and a direction arrow <ArrowUp /> or <ArrowDown />
+			</p>
 			<div className="attribute-boxes">
 				{" "}
 				<Correct /> <Close />
 				<Incorrect />
 			</div>
-			<p>Runtime of the movie</p>{" "}
+			<p>Runtime of the movie</p>
+			<p>
+				If you are within 15 minutes it will show close (yellow) and an
+				indicator and a direction arrow <ArrowUp /> or <ArrowDown />
+			</p>
 			<div className="attribute-boxes">
 				<Correct /> <Close />
 				<Incorrect />
